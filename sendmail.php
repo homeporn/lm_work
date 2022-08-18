a1efc6aebbfdb8fa0b7f996a5fbed85c9a843635
Vlad <you@example.comvmsaksonov@gmail.com>
2022-08-18 23:23:50 +0400

    php fix

--- sendmail.php
+++ sendmail.php
@@ -8,7 +8,16 @@ require 'mailer/src/PHPMailer.php';
 $mail = new PHPMailer (true);
 $mail -> Charset = "UTF-8"
 $mail -> setLangueage('ru', 'mailer/language/');
-$mail ->IsHTML(true);
+$mail -> IsHTML(true);
+
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+$mail->isSMTP();                                            //Send using SMTP
+$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+$mail->Username   = 'landmarksiteform@gmail.com';                     //SMTP username
+$mail->Password   = 'aetftmllqbzvrdlx';                               //SMTP password
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+$mail->Port       = 587;
 
 $mail -> setFrom('landmarksiteform@gmail.com', 'Новая заявка на сайте');
 $mail -> addAddress('inboxlmfostsite@gmail.com');
