c4ec3a9413d537fff848289c6621d8cd0b117376
Vlad <you@example.comvmsaksonov@gmail.com>
2022-08-19 10:45:17 +0400

    script off

--- sendmail.php
+++ sendmail.php
@@ -1,27 +1,28 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 
-require 'mailer/src/Exception.php';
 require 'mailer/src/PHPMailer.php';
+require 'mailer/src/SMTP.php'
+require 'mailer/src/Exception.php';
 
 $mail = new PHPMailer (true);
-$mail -> Charset = "UTF-8"
+$mail -> CharSet = "UTF-8"
 $mail -> setLangueage('ru', 'mailer/language/');
 $mail -> IsHTML(true);
 
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
 $mail->isSMTP();                                            //Send using SMTP
 $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
 $mail->Username   = 'landmarksiteform@gmail.com';                     //SMTP username
 $mail->Password   = 'aetftmllqbzvrdlx';                               //SMTP password
-$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-$mail->Port       = 587;
+$mail->SMTPSecure = "tls"            //Enable implicit TLS encryption
+$mail->Port       = 465;
 
 $mail -> setFrom('landmarksiteform@gmail.com', 'Новая заявка на сайте');
 $mail -> addAddress('inboxlmfostsite@gmail.com');
-$mail -> Subject = 'Новая заявка на сайте';
+$mail -> Subject = "Новая заявка на сайте";
 
 $body = 'Данные из формы на сайте:';
 
@@ -43,7 +44,7 @@ if(!$mail->send()){
     $message = 'Данные отправлены!';
 }
 
-$response = ['massage' => $message]
+$response = ['message' => $message]
 
 header('Content-type: application/json');
 echo json_encode($response);
